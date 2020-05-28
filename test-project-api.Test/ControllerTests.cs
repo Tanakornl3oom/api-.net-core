d3f9678e90913d049313759df212d866f5d1f3b1
tanakorn <tanakorn@central.tech>
2020-05-28 13:59:36 +0700

    add test mock service

---
+++ test-project-api.Test/ControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using test_project_api.Controllers;
+using test_project_api.Entity.models;
+using test_project_api.Service;
+using Xunit;
+
+namespace test_project_api.Test
+{
+    public class ControllerTests
+    {
+        private UserController userController;
+
+        private Mock<IQueryUser> _mockUserService;
+        public ControllerTests()
+        {
+            _mockUserService = new Mock<IQueryUser>();
+            userController = new UserController(_mockUserService.Object);
+        }
+
+
+        [Fact]
+        public void Get()
+        {
+            //Arrange
+            var users = new List<User>() { new User { Id = 0 } };
+
+            //mock service
+            _mockUserService.Setup(_ => _.getUsers()).Returns(users);
+
+            //Act
+            var response = userController.Get() as ObjectResult;
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)response.StatusCode);
+            Assert.Equal(users,response.Value);
+        }
+
+    }
+}
---
+++ test-project-api.Test/Repositories.cs
@@ -0,0 +1,47 @@
+using System;
+using test_project_api.Repositores;
+using Xunit;
+
+namespace test_project_api.Test
+{
+    public class RepositoriesTest
+    {
+        private IUserRepository _repository;
+
+        public RepositoriesTest()
+        {
+            _repository = new UserRepository();
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("Test", "Test");
+        }
+
+        [Fact]
+        public void GetUserNull()
+        {
+            // Act
+            var result = _repository.Get(1);
+            // Assert
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+        public void GetUserNotNull()
+        {
+            // Act
+            var result = _repository.Get(2);
+            // Assert
+            Assert.NotNull(result);
+
+            Assert.True(result.GetType().GetProperty("Id") != null && result.Id > 0);
+            Assert.True(result.GetType().GetProperty("Name") != null );
+            Assert.True(result.GetType().GetProperty("created") != null && result.created == DateTime.MinValue);
+
+        }
+    }
+}
